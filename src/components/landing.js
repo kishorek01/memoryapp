25541e61ec511f1f369988d22dba0e0fc726207c
kishorekumargit <nkishorekumar10@gmail.com>
2021-12-31 17:35:26 +0530

    Login api added

--- src/components/landing.js
+++ src/components/landing.js
@@ -16,7 +16,7 @@ export default class Landing extends Component{
         password:""
       },
     };
-    this.checkuser();
+    // this.checkuser();
   }
   changeValue(name,evt){
     let state=this.state;
@@ -77,42 +77,42 @@ pauseOnFocusLoss
 draggable
 pauseOnHover
 />
-      <div class="container" id="container">
-	<div class="form-container sign-up-container">
-		<form >
+      <div className="container" id="container">
+	<div className="form-container sign-up-container">
+		<form className="form">
 			<h1>Create Account</h1>
-			<input value={this.state.username} onChange={(e)=>{this.changeValue("username",e)}} placeholder="Username" />
-			<input value={this.state.password} onChange={(e)=>{this.changeValue("password",e)}} type="password" placeholder="Password" />
-			<button onClick={()=>this.signup()}>Sign Up</button>
+			<input className="inputcls" value={this.state.username} onChange={(e)=>{this.changeValue("username",e)}} placeholder="Username" />
+			<input className="inputcls" value={this.state.password} onChange={(e)=>{this.changeValue("password",e)}} type="password" placeholder="Password" />
+			<button className="button" onClick={()=>this.signup()}>Sign Up</button>
 		</form>
 	</div>
-	<div class="form-container sign-in-container">
-		<form >
+	<div className="form-container sign-in-container">
+		<form className="form">
 			<h1>Sign in</h1>
-			<input value={this.state.username} onChange={(e)=>{this.changeValue("username",e)}} placeholder="Username" />
-			<input value={this.state.password} onChange={(e)=>{this.changeValue("password",e)}} type="password" placeholder="Password" />
-			<button onClick={()=>this.login()}>Sign In</button>
+			<input className="inputcls" value={this.state.username} onChange={(e)=>{this.changeValue("username",e)}} placeholder="Username" />
+			<input className="inputcls" value={this.state.password} onChange={(e)=>{this.changeValue("password",e)}} type="password" placeholder="Password" />
+			<button className="button" onClick={()=>this.login()}>Sign In</button>
 		</form>
 	</div>
-	<div class="overlay-container">
-		<div class="overlay">
-			<div class="overlay-panel overlay-left">
+	<div className="overlay-container">
+		<div className="overlay">
+			<div className="overlay-panel overlay-left">
 				<h1>Welcome Back!</h1>
 				<p>To keep connected with us please login with your personal info</p>
-				<button class="ghost" onClick={()=>{this.signupclicked()}} id="signIn">Sign In</button>
+				<button className={"ghostbutton"} onClick={()=>{this.signupclicked()}} id="signIn">Sign In</button>
 			</div>
-			<div class="overlay-panel overlay-right">
+			<div className="overlay-panel overlay-right">
 				<h1>Hello, Friend!</h1>
 				<p>Enter your personal details and start journey with us</p>
-				<button class="ghost" onClick={()=>{this.signinclicked()}} id="signUp">Sign Up</button>
+				<button className={"ghostbutton"} onClick={()=>{this.signinclicked()}} id="signUp">Sign Up</button>
 			</div>
 		</div>
 	</div>
 </div>
 
-<footer>
+<footer className="footer">
 	<p>
-		Created with <i class="fa fa-heart"></i> by Kishore
+		Created with <i className="fa fa-heart"></i> by K
 	</p>
 </footer>
       </div>
