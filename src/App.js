bd0b9a6e183258e19eb69545d82d64a4a5fffa79
kishorekumargit <nkishorekumar10@gmail.com>
2021-12-31 16:41:41 +0530

    Memory App Login and Signup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import * as React from "react";
 import Main from "./main/main";
 import { BrowserRouter as Router} from "react-router-dom";
 import './App.css';
-import Parse from 'parse/dist/parse.min.js';
 
-// Your Parse initialization configuration goes here
-const PARSE_APPLICATION_ID = '2qWUxzgpSftXlbGyc5eC6L0b1dmsRV7UX71kRcPR';
-const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
-const PARSE_JAVASCRIPT_KEY = 'XpjIUh9YJwUqdecLvcWhz58EayQ25esycORLigdf';
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
 class App extends React.Component {
 
   render(){
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,73 +1,120 @@
 import React,{Component} from "react";
 import "./landing.css";
-import { GoogleLogin,GoogleLogout } from 'react-google-login';
-import Parse from 'parse/dist/parse.min.js';
-
-const client_id="1009752487014-bic5dodret05eode81rkoecfk1pg0c3n.apps.googleusercontent.com";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 
 export default class Landing extends Component{
   constructor(props){
     super(props);
     this.state = {
       isLoggedIn: false,
+      username: "",
+      password:"",
       userInfo: {
-        name: "",
-        emailId: "",
+        username: "",
+        password:""
       },
     };
     this.checkuser();
-    this.responseGoogle=this.responseGoogle.bind(this);
+  }
+  changeValue(name,evt){
+    let state=this.state;
+    state[name]=evt.target.value;
+    this.setState(state);
   }
   checkuser(){
     if(localStorage.getItem("token")){
       this.props.history.push("/home");
-      //
+
     }
   }
-  async addPerson() {
-   try {
-     const Person = new Parse.Object('User');
-     Person.set('name', 'John');
-     Person.set('email', 'john@back4app.com');
-     await Person.save();
-     this.checkuser();
-   } catch (error) {
-     console.log('Error saving new person: ', error);
-   }
- }
-  responseGoogle(response){
-    if(response){
-      let userInfo = {
-     name: response.profileObj.name,
-     emailId: response.profileObj.email,
-   };
-   localStorage.setItem("token",JSON.stringify(response.tokenObj));
-   localStorage.setItem("userInfo",JSON.stringify(userInfo));
-   this.setState({ userInfo, isLoggedIn: true },()=>{
-
-     this.addPerson();
-   });
+  signinclicked(){
+    const container = document.getElementById('container');
+    container.classList.add("right-panel-active");
+  }
+  signupclicked(){
+    const container = document.getElementById('container');
+    container.classList.remove("right-panel-active");
+  }
+  async login(){
+    let data={
+      username:this.state.username,
+      password:this.state.password
     }
-    console.log(response);
-    console.log(this.state);
-
+    await axios.post('https://gamebackend-black.vercel.app/api/auth/login',data)
+        .then((res) => {
+          toast(res.message);
+        })
+        .catch((err) => {
+            toast.error(err.message);
+        })
+  }
+  async signup(){
+    let data={
+      username:this.state.username,
+      password:this.state.password
+    }
+    await axios.post('https://gamebackend-black.vercel.app/api/auth/signup',data)
+        .then((res) => {
+          toast(res.message);
+        })
+        .catch((err) => {
+          toast.error(err.message);
+        })
   }
   render(){
     return(
-      <div className={"landdiv"}>
-      <div className={"firstdiv"}>
-      </div>
       <div>
-      <GoogleLogin
-    clientId={client_id}
-    buttonText="Login"
-    onSuccess={this.responseGoogle}
-    onFailure={this.responseGoogle}
-    cookiePolicy={'single_host_origin'}
-  />
+      <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+/>
+      <div class="container" id="container">
+	<div class="form-container sign-up-container">
+		<form action="#">
+			<h1>Create Account</h1>
+			<input value={this.state.username} onChange={(e)=>{this.changeValue("username",e)}} placeholder="Username" />
+			<input value={this.state.password} onChange={(e)=>{this.changeValue("password",e)}} type="password" placeholder="Password" />
+			<button onClick={()=>this.signup()}>Sign Up</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#">
+			<h1>Sign in</h1>
+			<input value={this.state.username} onChange={(e)=>{this.changeValue("username",e)}} placeholder="Username" />
+			<input value={this.state.password} onChange={(e)=>{this.changeValue("password",e)}} type="password" placeholder="Password" />
+			<button onClick={()=>this.login()}>Sign In</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Welcome Back!</h1>
+				<p>To keep connected with us please login with your personal info</p>
+				<button class="ghost" onClick={()=>{this.signupclicked()}} id="signIn">Sign In</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Hello, Friend!</h1>
+				<p>Enter your personal details and start journey with us</p>
+				<button class="ghost" onClick={()=>{this.signinclicked()}} id="signUp">Sign Up</button>
+			</div>
+		</div>
+	</div>
+</div>
 
-      </div>
+<footer>
+	<p>
+		Created with <i class="fa fa-heart"></i> by Kishore
+	</p>
+</footer>
       </div>
     );
   }
